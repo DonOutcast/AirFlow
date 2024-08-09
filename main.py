1a7c7059bd082b89273d930f691930ae7978a935
DonOutcast <sham1996@yandex.ru>
2024-08-09 21:07:12 +0300

    Новая ветка

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("Hello, World!")
+
+# git add main.py
+# git commit -m "Твой комментарий"
+# git branch Дает название ветки
+# git push origin <Название ветки>
+# git checkout -b <Название новой ветки>
